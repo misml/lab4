c3ca1f9f71a62584553bb91d177d17a99bd8e1ca
Patryk <theotpvanye@gmail.com>
2022-07-15 16:37:56 +0200

    add --logs 30

--- skrypt.sh
+++ skrypt.sh
@@ -1,8 +1,18 @@
 #!/bin/bash 
 case "$1" in
 	--data) echo "dzisiejsza data to: $(date)";;
-	--logs)for (( c=1; c<=100; c++))
+	--logs) if [ "$2" == "30" ]
+				then
+				for (( c=1; c<=$2; c++))
 					do 
 						echo "plik$c.txt, $0, $(date)" > plik$c.txt
-					done;;
+					done
+			elif [ "$1" == "--logs" ]
+				then 
+				for (( c=1; c<=100; c++))
+					do 
+						echo "plik$c.txt, $0, $(date)" > plik$c.txt
+					done
+			fi
+			;;
 esac
