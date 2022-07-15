2174845e84ea62cb3640c63ca2cbbff60437b52f
Patryk <theotpvanye@gmail.com>
2022-07-15 17:33:23 +0200

    add clone repository

--- skrypt.sh
+++ skrypt.sh
@@ -5,20 +5,23 @@ case "$1" in
 				then
 				for (( c=1; c<=$2; c++))
 					do 
-						echo "plik$c.txt, $0, $(date)" > plik$c.txt
+						echo "log$c.txt, $0, $(date)" > log$c.txt
 					done
 			elif [ "$1" == "--logs" ]
 				then 
 				for (( c=1; c<=100; c++))
 					do 
-						echo "plik$c.txt, $0, $(date)" > plik$c.txt
+						echo "log$c.txt, $0, $(date)" > log$c.txt
 					done
 			fi
 			;;
 	--help|-h) echo "Dostepne opcje:"
 			echo
-			echo "--data"
-			echo "--logs"
-			echo "--logs 30"
-			echo "--help";;
+			echo "--data lub -d"
+			echo "--logs lub -l"
+			echo "--logs 30 lub -l 30"
+			echo "--help lub -h"
+			echo "--init lub -i";;
+	--init|-i) git clone https://github.com/misml/lab4.git
+			echo "klonowanie sie powiodlo";;
 esac
